c9381ddf27ea279fcd9cb44d45a1b21120b3ec8d
tonyykosseifyy <tonykosseify123@gmail.com>
2021-10-12 12:57:06 +0300

    posted the first user

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,5 +3,14 @@ const User = require("../models/userSchema.js") ;
 
 
 exports.register = async ( req , res ) => {
-    console.log("register") ;
+    const { name , email , password } = req.body ;
+    try {
+        const user = new User({ name , email , password }) ;
+        const newUser = await user.save()
+        newUser._id = null ;
+        return res.status(201).json(newUser) ;
+    } catch (err) {
+        return res.status(400).send(err)
+    }
 };
+
